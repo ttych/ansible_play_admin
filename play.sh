9187c16c724f1e499b1b491ec8ae8f5bb7637ca3
Thomas Tych <thomas.tych@gmail.com>
2020-06-28 17:29:07 +0200

    save

--- play.sh
+++ play.sh
@@ -3,4 +3,5 @@
 SCRIPT_NAME="${0##*/}"
 SCRIPT_DIR=`cd "${0%$SCRIPT_NAME}" ; pwd`
 
-ansible-playbook -i "$SCRIPT_DIR/inventory" -l "$1" "$2"
+#ansible-playbook -i "$SCRIPT_DIR/inventory" -l "$1" "$2"
+ansible-playbook -l "$1" "$2"
